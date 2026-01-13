3f4702caf8c45223acd1f598584ced53a3140928
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-13 16:05:10 +0530

    Add files via upload

---
+++ A_Buttons.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (c & 1) {
+            if (b > a) {
+                cout << "Second\n";
+            }
+            else {
+                cout << "First\n";
+            }
+        }
+        else {
+            if (a > b) {
+                cout << "First\n";
+            }
+            else {
+                cout << "Second\n";
+            }
+        }
+    }
+}
