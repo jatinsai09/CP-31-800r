3fe32b0f8dfddf0e6503d56a061ccd21d711658d
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-13 16:08:51 +0530

    Add files via upload

---
+++ A_Array_Coloring.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector < int > v(n);
+        int oddcount = 0;
+        for (int i = 0; i < n; ++i) {
+            cin >> v[i];
+            if (v[i] & 1) {
+                ++oddcount;
+            }
+        }
+        if (oddcount & 1) {
+            cout << "No\n";
+        }
+        else {
+            cout << "Yes\n";
+        }
+    }
+
+}
