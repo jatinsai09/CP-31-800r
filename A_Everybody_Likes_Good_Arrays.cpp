b0385e7ba6c7c7cb71b1d90bb838511a145582ee
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-13 17:17:41 +0530

    Add files via upload

---
+++ A_Everybody_Likes_Good_Arrays.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector < int > v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        int c = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (abs(v[i] + v[i + 1]) % 2 == 0) {
+                c++;
+            }
+        }
+        cout << c << endl;
+    }
+}
